b48181fa04af44cb8a5018f6a1257ddf43f281aa
asha joseph <ashajo183@gmail.com>
2021-04-26 14:14:29 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/petclinic_practice/PetclinicPracticeApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.petclinic_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/petclinic_practice/PetclinicPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.petclinic_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
